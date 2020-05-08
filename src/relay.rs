35eba4daf15092483f5a84ff05631c83c151cdd6
zhboner <zhboner@gmail.com>
2020-05-09 07:18:52 +0800

    Improve the relay mechanism

--- src/relay.rs
+++ src/relay.rs
@@ -30,80 +30,93 @@ fn handle_client(mut stream: TcpStream, remote_socket: SocketAddr) {
     let (forward_tx, forward_rx) = mpsc::channel();
     let (backward_tx, backward_rx) = mpsc::channel();
 
-    stream.set_read_timeout(None).expect("set_read_timeout call failed");
-    stream.set_write_timeout(None).expect("set_read_timeout call failed");
+    // stream.set_read_timeout(None).expect("set_read_timeout call failed");
+    // stream.set_write_timeout(None).expect("set_read_timeout call failed");
     let mut cl_stream = stream;
     let mut lc_stream = cl_stream.try_clone().expect("Failed to clone client stream");
 
     let mut lr_stream = TcpStream::connect(remote_socket).expect("Failed to connect to remote host");
-    lr_stream.set_read_timeout(None).expect("set_read_timeout call failed");
-    lr_stream.set_write_timeout(None).expect("set_read_timeout call failed");
+    // lr_stream.set_read_timeout(None).expect("set_read_timeout call failed");
+    // lr_stream.set_write_timeout(None).expect("set_read_timeout call failed");
     let mut rl_stream = lr_stream.try_clone().expect("Failed to clone remote stream");
 
 
     thread::spawn(move || {             // client -> local
         let mut data = [0 as u8; BUFFER_SIZE];   // using 2048 byte buffer
-        // loop {
-        //     let size = cl_stream.read(&mut data).unwrap();
-        //     if size == 0 {continue;}
-        //     println!("read from client: {:?}", &data[..size]);
-        //     forward_tx.send((data, size));
-        //     println!("forward channel add");
-        // }
         loop {
             match cl_stream.read(&mut data) {
                 Ok(size) => {
+                    if let Err(_) = forward_tx.send((data, size)) {break}
                     if size == 0 {
-                        thread::sleep(Duration::from_millis(1));
-                        continue
+                        // thread::sleep(Duration::from_millis(1));
+                        break
                     }
-                    
-                    forward_tx.send((data, size)).unwrap();
                 },
                 Err(e) => {
-                    println!("Failed to read from client, error is {}", e);
-                    return
+                    forward_tx.send((data, 0)).unwrap();
+                    // println!("Failed to read from client, {}", e);
+                    break
                 }
             }
         }
-        
+        cl_stream.shutdown(Shutdown::Read);        
     });
 
     thread::spawn(move || {             // local -> remote
         loop {
-            let (mut data, size) = forward_rx.recv().unwrap();
-            if let Err(_) = lr_stream.write(&mut data[..size]) {
-                return
+            if let Ok((mut data, size)) = forward_rx.recv() {
+                if size == 0 {
+                    break  
+                }
+                if let Err(_) = lr_stream.write(&mut data[..size]) {
+                    break
+                }
+            } else {
+                break
             }
+            
         }
+        lr_stream.shutdown(Shutdown::Write);
+        return  
     });
 
     thread::spawn(move || {             // remote -> local
-        let mut data = [0 as u8; BUFFER_SIZE];   // using 2048 byte buffer
+        let mut data = [0 as u8; BUFFER_SIZE];
         loop {
             match rl_stream.read(&mut data) {
                 Ok(size) => {
+                    if let Err(_) = backward_tx.send((data, size)) {break}
                     if size == 0 {
-                        thread::sleep(Duration::from_millis(1));
-                        continue
+                        // thread::sleep(Duration::from_millis(1));
+                        break
                     }
-                    backward_tx.send((data, size)).unwrap();
                 },
                 Err(e) => {
-                    println!("Failed to read from remote, error is {}", e);
-                    return
+                    backward_tx.send((data, 0)).unwrap();
+                    // println!("Failed to read from remote, {}", e);
+                    break
                 }
             } 
         }
+        rl_stream.shutdown(Shutdown::Read);
     });
 
     // local -> client
     loop {
-        let (mut data, size) = backward_rx.recv().unwrap();
-        if let Err(_) = lc_stream.write(&mut data[..size]) {
-            return
+        if let Ok((mut data, size)) = backward_rx.recv() {
+            if size == 0 {
+                break  
+            }
+            if let Err(_) = lc_stream.write(&mut data[..size]) {
+                break
+            }
+        } else {
+            break
         }
+        
     }
+    lc_stream.shutdown(Shutdown::Write);   
+    return
 }
 
     
