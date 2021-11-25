4020428717a10b9498c25c3fd9c33a0db08692f5
zephyr <i@zephyr.moe>
2021-11-25 22:51:19 +0900

    add -h and -v; #1

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -13,83 +13,108 @@ pub enum CmdInput {
 }
 
 pub fn scan() -> CmdInput {
-    let matches = App::new("Realm")
+    let version = format!("{} {}", VERSION, FEATURES);
+    let app = App::new("Realm")
         .about("A high efficiency relay tool")
-        .version(format!("{} {}", VERSION, FEATURES).as_str())
-        .license("MIT")
+        .version(version.as_str())
+        .license("MIT");
+
+    let app = app
         .setting(AppSettings::ArgRequiredElseHelp)
         .setting(AppSettings::DisableVersionFlag)
         .setting(
             AppSettings::DisableHelpFlag | AppSettings::DisableHelpSubcommand,
         )
-        .override_usage("realm [FLAGS] [OPTIONS]")
-        .help_heading("FLAGS")
-        .args(&[
-            Arg::new("udp")
-                .short('u')
-                .long("udp")
-                .about("enable udp forward")
-                .display_order(0),
-            Arg::new("fast_open")
-                .short('f')
-                .long("tfo")
-                .about("enable tcp fast open")
-                .display_order(1),
-            Arg::new("zero_copy")
-                .short('z')
-                .long("splice")
-                .about("enable tcp zero copy")
-                .display_order(2),
-            Arg::new("daemon")
-                .short('d')
-                .long("daemon")
-                .about("run as a unix daemon")
-                .display_order(3),
-        ])
-        .help_heading("OPTIONS")
-        .args(&[
-            Arg::new("config")
-                .short('c')
-                .long("config")
-                .about("use config file")
-                .value_name("path")
-                .takes_value(true)
-                .display_order(0),
-            Arg::new("local")
-                .short('l')
-                .long("listen")
-                .about("listen address")
-                .value_name("addr")
-                .takes_value(true)
-                .display_order(1),
-            Arg::new("remote")
-                .short('r')
-                .long("remote")
-                .about("remote address")
-                .value_name("addr")
-                .takes_value(true)
-                .display_order(2),
-            Arg::new("through")
-                .short('x')
-                .long("through")
-                .about("send through ip or address")
-                .value_name("addr")
-                .takes_value(true)
-                .display_order(3),
-            Arg::new("tcp_timeout")
-                .long("tcp-timeout")
-                .about("set timeout value for tcp")
-                .value_name("second")
-                .takes_value(true)
-                .display_order(4),
-            Arg::new("udp_timeout")
-                .long("udp-timeout")
-                .about("set timeout value for udp")
-                .value_name("second")
-                .takes_value(true)
-                .display_order(5),
-        ])
-        .get_matches();
+        .override_usage("realm [FLAGS] [OPTIONS]");
+
+    let app = app.help_heading("FLAGS").args(&[
+        Arg::new("udp")
+            .short('u')
+            .long("udp")
+            .about("enable udp forward")
+            .display_order(0),
+        Arg::new("fast_open")
+            .short('f')
+            .long("tfo")
+            .about("enable tcp fast open")
+            .display_order(1),
+        Arg::new("zero_copy")
+            .short('z')
+            .long("splice")
+            .about("enable tcp zero copy")
+            .display_order(2),
+        Arg::new("daemon")
+            .short('d')
+            .long("daemon")
+            .about("run as a unix daemon")
+            .display_order(3),
+    ]);
+
+    let app = app.help_heading("OPTIONS").args(&[
+        Arg::new("help")
+            .short('h')
+            .long("help")
+            .about("show help")
+            .display_order(0),
+        Arg::new("version")
+            .short('v')
+            .long("version")
+            .about("show version")
+            .display_order(1),
+        Arg::new("config")
+            .short('c')
+            .long("config")
+            .about("use config file")
+            .value_name("path")
+            .takes_value(true)
+            .display_order(2),
+        Arg::new("local")
+            .short('l')
+            .long("listen")
+            .about("listen address")
+            .value_name("addr")
+            .takes_value(true)
+            .display_order(3),
+        Arg::new("remote")
+            .short('r')
+            .long("remote")
+            .about("remote address")
+            .value_name("addr")
+            .takes_value(true)
+            .display_order(4),
+        Arg::new("through")
+            .short('x')
+            .long("through")
+            .about("send through ip or address")
+            .value_name("addr")
+            .takes_value(true)
+            .display_order(5),
+        Arg::new("tcp_timeout")
+            .long("tcp-timeout")
+            .about("set timeout value for tcp")
+            .value_name("second")
+            .takes_value(true)
+            .display_order(6),
+        Arg::new("udp_timeout")
+            .long("udp-timeout")
+            .about("set timeout value for udp")
+            .value_name("second")
+            .takes_value(true)
+            .display_order(7),
+    ]);
+
+    let mut xapp = app.clone();
+    let matches = app.get_matches();
+
+    if matches.is_present("help") {
+        xapp.print_help().unwrap();
+        return CmdInput::None;
+    }
+
+    if matches.is_present("version") {
+        print!("{}", xapp.render_version());
+        return CmdInput::None;
+    }
 
     parse_matches(matches)
 }
