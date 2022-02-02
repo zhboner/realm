6a8b86b10b64f5a5fcd3b0ae2ad35918c648bacb
zephyr <i@zephyr.moe>
2022-02-02 23:43:11 +0900

    update deps

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -19,32 +19,32 @@ fn add_flags(app: App) -> App {
         Arg::new("help")
             .short('h')
             .long("help")
-            .about("show help")
+            .help("show help")
             .display_order(0),
         Arg::new("version")
             .short('v')
             .long("version")
-            .about("show version")
+            .help("show version")
             .display_order(1),
         Arg::new("daemon")
             .short('d')
             .long("daemon")
-            .about("run as a unix daemon")
+            .help("run as a unix daemon")
             .display_order(2),
         Arg::new("use_udp")
             .short('u')
             .long("udp")
-            .about("force enable udp forward")
+            .help("force enable udp forward")
             .display_order(3),
         Arg::new("fast_open")
             .short('f')
             .long("tfo")
-            .about("force enable tcp fast open")
+            .help("force enable tcp fast open")
             .display_order(4),
         Arg::new("zero_copy")
             .short('z')
             .long("splice")
-            .about("force enable tcp zero copy")
+            .help("force enable tcp zero copy")
             .display_order(5),
     ])
 }
@@ -54,28 +54,28 @@ fn add_options(app: App) -> App {
         Arg::new("config")
             .short('c')
             .long("config")
-            .about("use config file")
+            .help("use config file")
             .value_name("path")
             .takes_value(true)
             .display_order(0),
         Arg::new("local")
             .short('l')
             .long("listen")
-            .about("listen address")
+            .help("listen address")
             .value_name("addr")
             .takes_value(true)
             .display_order(1),
         Arg::new("remote")
             .short('r')
             .long("remote")
-            .about("remote address")
+            .help("remote address")
             .value_name("addr")
             .takes_value(true)
             .display_order(2),
         Arg::new("through")
             .short('x')
             .long("through")
-            .about("send through ip or address")
+            .help("send through ip or address")
             .value_name("addr")
             .takes_value(true)
             .display_order(3),
@@ -86,43 +86,43 @@ fn add_global_options(app: App) -> App {
     app.help_heading("GLOBAL OPTIONS").args(&[
         Arg::new("log_level")
             .long("log-level")
-            .about("override log level")
+            .help("override log level")
             .value_name("level")
             .takes_value(true)
             .display_order(0),
         Arg::new("log_output")
             .long("log-output")
-            .about("override log output")
+            .help("override log output")
             .value_name("path")
             .takes_value(true)
             .display_order(1),
         Arg::new("dns_mode")
             .long("dns-mode")
-            .about("override dns mode")
+            .help("override dns mode")
             .value_name("mode")
             .takes_value(true)
             .display_order(2),
         Arg::new("dns_protocol")
             .long("dns-protocol")
-            .about("override dns protocol")
+            .help("override dns protocol")
             .value_name("protocol")
             .takes_value(true)
             .display_order(3),
         Arg::new("dns_servers")
             .long("dns-servers")
-            .about("override dns servers")
+            .help("override dns servers")
             .value_name("servers")
             .takes_value(true)
             .display_order(4),
         Arg::new("tcp_timeout")
             .long("tcp-timeout")
-            .about("override tcp timeout")
+            .help("override tcp timeout")
             .value_name("second")
             .takes_value(true)
             .display_order(5),
         Arg::new("udp_timeout")
             .long("udp-timeout")
-            .about("override udp timeout")
+            .help("override udp timeout")
             .value_name("second")
             .takes_value(true)
             .display_order(6),
@@ -133,8 +133,7 @@ pub fn scan() -> CmdInput {
     let version = format!("{} {}", VERSION, FEATURES);
     let app = App::new("Realm")
         .about("A high efficiency relay tool")
-        .version(version.as_str())
-        .license("MIT");
+        .version(version.as_str());
 
     let app = app
         .setting(AppSettings::ArgRequiredElseHelp)
