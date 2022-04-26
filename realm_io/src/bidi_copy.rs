cd00d9296a43bd1ea041b18182dbccb5bb48bf47
zephyr <i@zephyr.moe>
2022-04-27 00:04:13 +0900

    realm_io: add optional brutal-shutdown feature

--- realm_io/src/bidi_copy.rs
+++ realm_io/src/bidi_copy.rs
@@ -118,12 +118,23 @@ where
         let a_to_b = transfer(cx, a_to_b, a, b, ab_amt)?;
         let b_to_a = transfer2::<B, SL, SR>(cx, b_to_a, b, a, ba_amt)?;
 
-        // It is not a problem if ready! returns early because transfer_one_direction for the
-        // other direction will keep returning TransferState::Done(count) in future calls to poll
-        ready!(a_to_b);
-        ready!(b_to_a);
+        // graceful shutdown
+        #[cfg(not(feature = "brutal-shutdown"))]
+        {
+            ready!(a_to_b);
+            ready!(b_to_a);
+            Poll::Ready(Ok(()))
+        }
 
-        Poll::Ready(Ok(()))
+        // brutal shutdown
+        #[cfg(feature = "brutal-shutdown")]
+        {
+            if a_to_b.is_ready() || b_to_a.is_ready() {
+                Poll::Ready(Ok(()))
+            }else {
+                Poll::Pending
+            }
+        }
     }
 }
 
