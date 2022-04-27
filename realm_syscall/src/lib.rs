2513f5c30edb8841d5b994d26a7bd3297331b45d
zephyr <i@zephyr.moe>
2022-04-27 21:37:09 +0900

    realm_syscall: fix conflicts on windows

--- realm_syscall/src/lib.rs
+++ realm_syscall/src/lib.rs
@@ -1,9 +1,14 @@
 //! Realm's convenient syscall collections.
 
+#[cfg(unix)]
 mod daemon;
-mod nofile;
-mod socket;
-
+#[cfg(unix)]
 pub use daemon::*;
+
+#[cfg(all(unix, not(target_os = "android")))]
+mod nofile;
+#[cfg(all(unix, not(target_os = "android")))]
 pub use nofile::*;
+
+mod socket;
 pub use socket::*;
