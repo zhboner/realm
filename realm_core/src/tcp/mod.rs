b27c11debf851203bd02096ee3357da7a8220c4d
zephyr <i@zephyr.moe>
2022-04-30 22:12:41 +0900

    realm_core: scoped concurrent

--- realm_core/src/tcp/mod.rs
+++ realm_core/src/tcp/mod.rs
@@ -50,3 +50,41 @@ pub async fn run_tcp(endpoint: Endpoint) -> Result<()> {
         });
     }
 }
+
+/// Launch a tcp relay, where tasks are not detached.
+#[cfg(feature = "x")]
+pub async fn run_tcp_scoped(endpoint: Endpoint) -> Result<()> {
+    use tokio::task::JoinSet;
+    let Endpoint {
+        laddr,
+        raddr,
+        conn_opts,
+    } = endpoint;
+
+    let raddr = Ref::new(&raddr);
+    let conn_opts = Ref::new(&conn_opts);
+
+    let lis = socket::bind(&laddr).unwrap_or_else(|e| panic!("[tcp]failed to bind {}: {}", &laddr, e));
+
+    let mut tasks = JoinSet::new();
+
+    loop {
+        let (local, addr) = match lis.accept().await {
+            Ok(x) => x,
+            Err(e) => {
+                log::error!("[tcp]failed to accept: {}", e);
+                continue;
+            }
+        };
+
+        // ignore error
+        let _ = local.set_nodelay(true);
+
+        tasks.spawn(async move {
+            match connect_and_relay(local, raddr, conn_opts).await {
+                Ok(..) => log::debug!("[tcp]{} => {}, finish", addr, raddr.as_ref()),
+                Err(e) => log::error!("[tcp]{} => {}, error: {}", addr, raddr.as_ref(), e),
+            }
+        });
+    }
+}
