0ffe86ecbd6319052cb73e6844430a46a80a3cff
zephyr <i@zephyr.moe>
2022-05-18 07:40:30 +0900

    use fallback if splice is not supported on linux, close #63, close #66

--- realm_core/src/tcp/plain.rs
+++ realm_core/src/tcp/plain.rs
@@ -1,11 +1,19 @@
 use std::io::Result;
 use tokio::net::TcpStream;
 
+const EINVAL: i32 = 22;
+
 #[inline]
 pub async fn run_relay(mut local: TcpStream, mut remote: TcpStream) -> Result<()> {
     #[cfg(all(target_os = "linux"))]
     {
-        realm_io::bidi_zero_copy(&mut local, &mut remote).await
+        match realm_io::bidi_zero_copy(&mut local, &mut remote).await {
+            Ok(()) => Ok(()),
+            Err(ref e) if e.raw_os_error().map_or(false, |ec| ec == EINVAL) => {
+                realm_io::bidi_copy(&mut local, &mut remote).await
+            }
+            Err(e) => Err(e),
+        }
     }
 
     #[cfg(not(target_os = "linux"))]
