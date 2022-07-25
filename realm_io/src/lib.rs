7eb60810b2c789758c9d497c546adc881242674b
zephyr <i@zephyr.moe>
2022-07-26 01:05:13 +0900

    io: use stabilized ready! macro; make peek,statistic optional

--- realm_io/src/lib.rs
+++ realm_io/src/lib.rs
@@ -1,5 +1,3 @@
-#![feature(ready_macro)]
-
 //! Realm's high performance IO collections.
 //!
 //! ## Example
@@ -47,15 +45,18 @@ mod buf;
 mod mem_copy;
 mod bidi_copy;
 
-#[cfg(target_os = "linux")]
-mod zero_copy;
-
 pub use buf::{AsyncIOBuf, CopyBuffer};
 pub use bidi_copy::bidi_copy_buf;
 pub use mem_copy::{bidi_copy, buf_size, set_buf_size};
 
 #[cfg(target_os = "linux")]
+mod zero_copy;
+
+#[cfg(target_os = "linux")]
 pub use zero_copy::{Pipe, AsyncRawIO, bidi_zero_copy, pipe_size, set_pipe_size};
 
+#[cfg(feature = "peek")]
 pub mod peek;
+
+#[cfg(feature = "statistic")]
 pub mod statistic;
