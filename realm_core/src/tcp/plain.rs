c15f8ab91bf52693fbfc8505fa5308c959325558
zephyr <i@zephyr.moe>
2022-12-09 16:51:20 +0900

    use std::io::ErrorKind instead of errno

--- realm_core/src/tcp/plain.rs
+++ realm_core/src/tcp/plain.rs
@@ -1,17 +1,14 @@
 use std::io::Result;
 use tokio::net::TcpStream;
 
-const EINVAL: i32 = 22;
-
 #[inline]
 pub async fn run_relay(mut local: TcpStream, mut remote: TcpStream) -> Result<()> {
     #[cfg(all(target_os = "linux"))]
     {
+        use std::io::ErrorKind;
         match realm_io::bidi_zero_copy(&mut local, &mut remote).await {
             Ok(()) => Ok(()),
-            Err(ref e) if e.raw_os_error().map_or(false, |ec| ec == EINVAL) => {
-                realm_io::bidi_copy(&mut local, &mut remote).await
-            }
+            Err(ref e) if e.kind() == ErrorKind::InvalidInput => realm_io::bidi_copy(&mut local, &mut remote).await,
             Err(e) => Err(e),
         }
     }
